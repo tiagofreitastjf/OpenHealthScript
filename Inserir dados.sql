f9b4f6ab55dd6e0325b28c80f96f2e847d8ae608
Tiago Freitas <45900881+tiagofreitastjf@users.noreply.github.com>
2021-11-12 21:20:20 -0300

    Script para inserir dados

---
+++ Inserir dados.sql
@@ -0,0 +1,5 @@
+insert into Clinica values('Clinica teste', '12345-678', 123, 'Bairro', 'Rua da clinica', 'Complemento', 'Campinas', 'SP');
+insert into Cliente values(1, 'Cliente teste', 2000-01-01, 'cliente_teste@email.com', '123456', '000.000.000-00', '12345-678', 123, 'Bairro', 'Rua da clinica', 'Complemento', 'Campinas', 'SP', '');
+insert into Profissional values(1, 'Nome', 2000-01-01, 'profissional_teste@email.com', '123456', '111.111.111-11', '12345-678', 123, 'Bairro', 'Rua da clinica', 'Complemento', 'Campinas', 'SP');
+insert into Consulta values(1, 1, 1, 2021-01-01, 'Cardio', 'Descrição: Tudo normal.');
+insert into Vacina values(1, 1, 1, 2021-01-01, 'COVID', 'Descrição: vacinado.');
