3d52b4025df8234707e4b5dbe596ee72c59d3dda
Tiago Freitas <45900881+tiagofreitastjf@users.noreply.github.com>
2021-11-19 19:50:23 -0300

    Atualizado

---
+++ Create Cliente.sql
@@ -0,0 +1,31 @@
+USE [OpenHealth]
+GO
+
+/****** Object:  Table [dbo].[Cliente]    Script Date: 19/11/2021 19:41:42 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Cliente](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[Nome] [varchar](200) NOT NULL,
+	[DataNascimento] [datetime] NOT NULL,
+	[Email] [varchar](100) NOT NULL,
+	[Senha] [varchar](20) NOT NULL,
+	[CPF] [varchar](14) NOT NULL,
+	[CEP] [varchar](9) NOT NULL,
+	[Numero] [int] NOT NULL,
+	[Bairro] [varchar](100) NOT NULL,
+	[Endereco] [varchar](150) NOT NULL,
+	[Complemento] [varchar](100) NULL,
+	[Cidade] [varchar](100) NOT NULL,
+	[Estado] [varchar](2) NOT NULL
+ CONSTRAINT [PK_Cliente] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
